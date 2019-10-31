5a125a83da403d687ac2a24177406cbcb260a46c
new <aji@gmail.com>
2019-10-31 17:13:37 +0100

    db connection

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     jwtSecret : process.env.JWT_SECRET || "your_secret_key",
-    mongoUri: process.env.MONGODB_URI || process.env.MONGODB_HOST || 'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGoDB_PORT || '27017') + '/project'
+    mongoUri: process.env.MONGODB_URI || process.env.MONGODB_HOST || 'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGODB_PORT || '27017') + '/project'
 }
 
 export default config;
--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ import app from './app';
 import mongoose from 'mongoose';
 
 mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri)
+const uri = "mongodb+srv://richard01:seun08167739200@richard01-xsh4r.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(uri)
+//mongoose.connect(config.mongoUri)
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`)
 })
