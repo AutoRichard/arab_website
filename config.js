f653395d32317971f97be7f8d30544df13a565e4
new <aji@gmail.com>
2019-10-31 18:20:49 +0100

    connect

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 8080,
     jwtSecret : process.env.JWT_SECRET || "your_secret_key",
     mongoUri: process.env.MONGODB_URI || process.env.MONGODB_HOST || 'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGODB_PORT || '27017') + '/project'
 }
