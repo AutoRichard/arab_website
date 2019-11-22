61184ad1d330e8a1188b70dac998f4a4f3afea60
new <aji@gmail.com>
2019-11-22 18:06:28 +0100

    update

--- server/controllers/forum.controller.js
+++ server/controllers/forum.controller.js
@@ -104,8 +104,11 @@ const update = (req, res, next) => {
 }
 
 const photo = (req, res) => {
-    res.set("Content-Type", req.details.photo.contentType);
-    return res.send(req.details.photo.data);
+    req.details.photo = undefined;
+    return res.json(req.details);
+
+    //res.set("Content-Type", req.details.photo.contentType);
+    //return res.send(req.details.photo.data);
 }
 
 //return specific forum
